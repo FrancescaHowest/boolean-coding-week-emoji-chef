5e89d9e456ae867589cb1cde7a86469d4235ced6
Francesca Demilde <francesca.demilde@student.howest.be>
2023-08-10 17:10:42 +0200

    Create ingredient tiles

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // Variables
 const NUMBEROFBOWLS = 3;
 
-let ingredients
+let ingredients = [];
 
 let divIngredients, divBowls;
 
@@ -40,28 +40,46 @@ function initialize() {
 
 async function getJSONData() {
     fetch("https://francescahowest.github.io/boolean-coding-week-emoji-chef/api/data.json")
+    .then(resp => resp.json())
     .then(data => {
         ingredients = data;
 
-        for (let ingredient in ingredients) {
-            console.log(ingredient);
-            createIngredientTile(ingredient);
+        console.log(ingredients);
+
+        let foodTypes = [];
+
+        for (let foodType in ingredients) {
+            const options = ingredients[foodType]["Options"];
+
+            if(Array.isArray(options)){
+                options.forEach(element => {
+                    createIngredientTile(element);
+                    console.log(element.name);
+                });
+            }
+
+            else
+                createIngredientTile(options);
         }
     })
     .catch(error => console.log(error));
 }
 
 function createIngredientTile(ingredient) {{
-    const divIngredient = document.createElement("div");
-    divIngredient.classList.add("ingredient");
-    divIngredient.textContent = ingredient["image"];
+    const divIngredientTile = document.createElement("div");
 
-    divIngredients.appendChild(divIngredient);
+    divIngredientTile.classList.add("ingredient");
+    divIngredientTile.textContent = ingredient.image;
+
+    divIngredientTile.addEventListener("click", "addIngredient");
+
+    divIngredients.appendChild(divIngredientTile);
 }}
 
 function createBowls() {
     for (let index = 0; index < NUMBEROFBOWLS; index++) {
         const divBowl = document.createElement("div");
+
         divBowl.classList.add("bowl-slot");
         divBowl.textContent = "?";
 
